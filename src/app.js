b6a2cfa0048189ff5e49d8a23aab76651b9e8587
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-11-14 22:27:20 +0300

    npm test fix

--- src/app.js
+++ src/app.js
@@ -4,20 +4,19 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/styles"));
- 
 // обработка запроса к корню веб-сайта
 app.get("/", function(request, response){
      
     response.send("styles/index.html");
-});
+    });
 
 app.get('/flex', function(req, res) {
   res.sendFile(path.join(__dirname, 'styles/indexFlex.html'));
-});
+    });
 
 
 app.get('/grid', function(req, res){
   res.sendFile(path.join(__dirname, 'styles/indexGrid.html'));
-});
+    });
 
 app.listen(process.env.PORT || 3000);
