1a19eab3027170f94c4d246ae44a33ebc5fc5859
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-11-14 22:05:38 +0300

    node fix

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ app.get("/", function(request, response){
      
     response.send("styles/index.html");
 });
+
+app.get("/flex", function(request, response){
+     
+  response.send("styles/indexFlex.html");
+});
+
+app.get("/grid", function(request, response){
+     
+  response.send("styles/indexGrid.html");
+});
+
 app.listen(process.env.PORT || 3000);
