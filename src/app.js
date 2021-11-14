67dab509b7b3160b88154e4a35324d2ed6c95e52
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-11-14 21:57:43 +0300

    express added

--- src/app.js
+++ src/app.js
@@ -1,57 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-http.createServer(function (request, response) {
-    console.log('request ', request.url);
-
-    var filePath = './styles/' + request.url;
-    if (filePath == './') {
-        filePath = './styles/index.html';
-    }
-
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm'
-    };
-
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
-}).listen(process.env.PORT);
-console.log('Server running at http://127.0.0.1:8125/');
+app.use(express.static(__dirname + "/styles"));
+ 
+// обработка запроса к корню веб-сайта
+app.get("/", function(request, response){
+     
+    response.send("styles/index.html");
+});
+app.listen(process.env.PORT || 3000);
